b6272357151503f923e57875b731ce552cb5577d
mmmaaaggg <mmmaaaggg@163.com>
2018-03-27 11:35:05 +0800

    add flask-login

---
+++ install_requirements.bat
@@ -0,0 +1,9 @@
+python -m pip install --upgrade pip
+# 部分包网络下载比较费劲，于是下载到本地进行安装
+pip install d:\Softwares\DevTools\Python\pytz-2018.3-py2.py3-none-any.whl
+pip install d:\Softwares\DevTools\Python\SQLAlchemy-1.2.5-cp35-cp35m-win_amd64.whl
+pip install d:\Softwares\DevTools\Python\pycryptodome-3.4.3-cp35-cp35m-win_amd64.whl
+pip install d:\Softwares\DevTools\Python\Babel-2.5.3-py2.py3-none-any.whl
+pip install d:\Softwares\DevTools\Python\Jinja2-2.10-py2.py3-none-any.whl
+pip install d:\Softwares\DevTools\Python\Werkzeug-0.14.1-py2.py3-none-any.whl
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
